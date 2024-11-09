3b22325ad46295ccaa0b52daa0ada609399ab901
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2024-11-09 17:29:22 +0530

    contest changes

--- src/validation/contest.ts
+++ src/validation/contest.ts
@@ -1,28 +1,29 @@
 import Joi from "joi";
 
 export const addContestSchema = Joi.object().keys({
-    contestName: Joi.string().required(),
-    contestType: Joi.string().required(),
-    contestStartDate: Joi.date().required(),
-    contestEndDate: Joi.date().required(),
-    totalSpots: Joi.number().required(),
-    fees: Joi.number().required(),
-    winningAmountPerFee: Joi.number().required(),
-    winnerPercentage: Joi.number().required(),
+    name: Joi.string().required(),
+    type: Joi.string().optional(),
+    startDate: Joi.date().optional(),
+    endDate: Joi.date().optional(),
+    totalSpots: Joi.number().optional(),
+    fees: Joi.number().optional(),
+    winningAmountPerFee: Joi.number().optional(),
+    winnerPercentage: Joi.number().optional(),
     ranks: Joi.array().items(Joi.object().keys({
-        place: Joi.string().required()
+        place: Joi.string().optional()
     })).optional(),
-    totalQuestions: Joi.number().required(),
-    totalTime: Joi.string().required(), // e.g., "2 hours"
-    totalMarks: Joi.number().required(),
+    totalQuestions: Joi.number().optional(),
+    totalTime: Joi.string().optional(), // e.g., "2 hours"
+    totalMarks: Joi.number().optional(),
+    classesId: Joi.string().optional()
 });
 
 export const editContestSchema = Joi.object().keys({
     contestId: Joi.string().required(),
-    contestName: Joi.string().optional(),
-    contestType: Joi.string().optional(),
-    contestStartDate: Joi.date().optional(),
-    contestEndDate: Joi.date().optional(),
+    name: Joi.string().optional(),
+    type: Joi.string().optional(),
+    startDate: Joi.date().optional(),
+    endDate: Joi.date().optional(),
     totalSpots: Joi.number().optional(),
     fees: Joi.number().optional(),
     winningAmountPerFee: Joi.number().optional(),
@@ -33,6 +34,7 @@ export const editContestSchema = Joi.object().keys({
     totalQuestions: Joi.number().optional(),
     totalTime: Joi.string().optional(),
     totalMarks: Joi.number().optional(),
+    classesId: Joi.string().optional()
 });
 
 export const deleteContestSchema = Joi.object().keys({
